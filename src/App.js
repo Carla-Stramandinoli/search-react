38dc86cea58bedf0e4817f8e0d3622499d55ef31
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2024-01-23 12:44:09 -0300

    search con react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SearchComponent from './components/searchComponent';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <h2 className='text-center'>React search</h2>
+      <SearchComponent />
+    </div>
+  );
+}
+
+export default App;
